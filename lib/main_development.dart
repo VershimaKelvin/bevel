2dcc915b1a77237657eb3ffef626aed8f590218d
VershimaKelvin <kelvinityavyar@gmail.com>
2022-11-16 16:59:15 +0100

    feat: integrated CI/CD with Github action

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -11,6 +11,7 @@ import 'package:bevel/core/di/di_container.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
   await bootstrap(
         () => const App(),
